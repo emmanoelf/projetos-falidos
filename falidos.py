2f7cc0ebe2be864e0a4d0b951a1f3b7f3fff39ea
Euller Lisowski <euller_sl@hotmail.com>
2017-03-05 12:26:19 -0300

    Ajuste no metodo run() para rodar na heroku

--- falidos.py
+++ falidos.py
@@ -110,4 +110,5 @@ def create_db():
 	con.commit()
 
 if __name__ == '__main__':
-	run(host="localhost", port=8080, debug=False, reloader=True)
+	#run(host="localhost", port=8080, debug=True, reloader=True)
+	run()

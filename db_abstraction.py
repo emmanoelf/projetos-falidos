24e1ca169af910969d86d3138fdabb57a61368c8
Euller Lisowski <euller_sl@hotmail.com>
2017-03-12 16:14:10 -0300

    Classe que faz a interação com o db

--- db_abstraction.py
+++ db_abstraction.py
@@ -1,24 +1,35 @@
+'''Classe que abstrai conexoes
+com sqlite3 e futuramente postgresql'''
+
 import sqlite3
 
-class worker:
-	"""docstring for worker"""
-	def __init__(self, table, statement, params=tuple()):
-		self.table = table
-		self.statement = statement
-		self.params = params
+
+class Worker(object):
+    '''Classe que recebe por parametro
+    o a tabela, o trecho e os parametros
+    para execucao de acoes com o
+    banco de dados'''
+    def __init__(self, table, statement, params=tuple()):
+        self.table = table
+        self.statement = statement
+        self.params = params
 
 
-	def query(self):
-		conn = sqlite3.connect(self.table)
-		cursor = conn.cursor()
-		result = cursor.execute(self.statement, self.params).fetchall()
-		cursor.close()
-		return result
+    def query(self):
+        '''Metodo responsavel por consultar
+        o banco de dados e retornar o resultado'''
+        conn = sqlite3.connect(self.table)
+        cursor = conn.cursor()
+        result = cursor.execute(self.statement, self.params).fetchall()
+        cursor.close()
+        return result
 
 
-	def insert(self):
-		conn = sqlite3.connect(self.table)
-		cursor = conn.cursor()
-		cursor.execute(self.statement, self.params)
-		conn.commit()
-		cursor.close()
+    def insert(self):
+        '''Metodo responsavel por inserir dados no
+        banco de dados'''
+        conn = sqlite3.connect(self.table)
+        cursor = conn.cursor()
+        cursor.execute(self.statement, self.params)
+        conn.commit()
+        cursor.close()

ff27259a933202029b7883029a808a72da694682
Euller Lisowski <euller_sl@hotmail.com>
2017-03-12 16:35:40 -0300

    Ajuste na rota create_db

--- falidos.py
+++ falidos.py
@@ -162,7 +162,8 @@ def create_db():
                 hope boolean NOT NULL)''')
 
     con.execute('''INSERT INTO user (name, email, user, password)
-                VALUES ('admin', 'admin@admin', 'admin', '')''')
+                VALUES ('admin', 'admin@admin', 'admin',
+                '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4')''')
     con.commit()
 
 
